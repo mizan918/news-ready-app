1221b8fd06eeee1ff06846e17dcb839d99a1d10d
mizan918 <52394124+mizan918@users.noreply.github.com>
2026-07-12 15:13:41 +0600

    Update app.py

--- news_ready_app/app.py
+++ news_ready_app/app.py
@@ -3,17 +3,37 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 newspapers = [
-    {"name": "Prothom Alo", "url": "https://www.prothomalo.com"},
-    {"name": "Daily Star", "url": "https://www.thedailystar.net"},
-    {"name": "BBC", "url": "https://www.bbc.com"},
-    {"name": "CNN", "url": "https://www.cnn.com"},
-    {"name": "Al Jazeera", "url": "https://www.aljazeera.com"},
-    {"name": "The Guardian", "url": "https://www.theguardian.com"},
-    {"name": "New York Times", "url": "https://www.nytimes.com"},
-    {"name": "Reuters", "url": "https://www.reuters.com"},
-    {"name": "Jugantor", "url": "https://www.jugantor.com"},
-    {"name": "Kaler Kantho", "url": "https://www.kalerkantho.com"},
+# 🇧🇩 Bangladeshi Newspapers
+{"name": "Prothom Alo", "url": "https://www.prothomalo.com"},
+{"name": "The Daily Star", "url": "https://www.thedailystar.net"},
+{"name": "Kaler Kantho", "url": "https://www.kalerkantho.com"},
+{"name": "Jugantor", "url": "https://www.jugantor.com"},
+{"name": "Bangladesh Pratidin", "url": "https://www.bd-pratidin.com"},
+{"name": "Ittefaq", "url": "https://www.ittefaq.com.bd"},
+{"name": "Samakal", "url": "https://www.samakal.com"},
+{"name": "Janakantha", "url": "https://www.dailyjanakantha.com"},
+{"name": "Amar Desh", "url": "https://www.amardesh.com"},
+{"name": "Inqilab", "url": "https://www.dailyinqilab.com"},
+{"name": "Naya Diganta", "url": "https://www.dailynayadiganta.com"},
+{"name": "Manab Zamin", "url": "https://mzamin.com"},
+{"name": "Bhorer Kagoj", "url": "https://www.bhorerkagoj.com"},
+{"name": "Dhaka Tribune", "url": "https://www.dhakatribune.com"},
+{"name": "Business Standard", "url": "https://www.tbsnews.net"},
+{"name": "Bangla Tribune", "url": "https://www.banglatribune.com"},
+{"name": "UNB", "url": "https://www.unb.com.bd"},
+
+```
+# 🌍 International (selected)
+{"name": "BBC", "url": "https://www.bbc.com"},
+{"name": "CNN", "url": "https://www.cnn.com"},
+{"name": "Al Jazeera", "url": "https://www.aljazeera.com"},
+{"name": "Reuters", "url": "https://www.reuters.com"},
+{"name": "The Guardian", "url": "https://www.theguardian.com"},
+{"name": "New York Times", "url": "https://www.nytimes.com"},
+```
+
 ]
+
 
 @app.route("/")
 def home():
